ae4d9e1b0abce15d49c41b5f7a961e4cf0a79d61
Qriist <qriist@gmail.com>
2025-03-02 05:06:30 -0500

    added libmagic as a dependency

--- build known bad.bat
+++ build known bad.bat
@@ -1,15 +1,11 @@
-DEL bin\*.dll
-DEL bin\*.exe
-DEL bin\*.mgc
+vcpkg remove curl --overlay-ports=overlays\openssl --x-install-root=build --recurse
+vcpkg remove wolfssl --x-install-root=build --recurse
 
-vcpkg remove curl --overlay-ports=C:\Projects\LibQurl\overlays\openssl --x-install-root=C:\Projects\LibQurl\bin --recurse
-vcpkg remove wolfssl --x-install-root=C:\Projects\LibQurl\bin --recurse
+vcpkg install wolfssl[asio,dtls,quic] --x-install-root=build --recurse
 
-vcpkg install wolfssl[asio,dtls,quic] --x-install-root=C:\Projects\LibQurl\bin --recurse
+vcpkg remove curl --overlay-ports=overlays\openssl --x-install-root=build --recurse
+vcpkg install curl[brotli,c-ares,gnutls,gsasl,http2,idn,idn2,ldap,mbedtls,non-http,openssl,psl,rtmp,schannel,ssh,ssl,sspi,tool,websockets,winidn,wolfssl,zstd]:x64-windows --overlay-ports=overlays\openssl --x-install-root=build --recurse
 
-vcpkg remove curl --overlay-ports=C:\Projects\LibQurl\overlays\openssl --x-install-root=C:\Projects\LibQurl\bin --recurse
-vcpkg install curl[brotli,c-ares,gnutls,gsasl,http2,idn,idn2,ldap,mbedtls,non-http,openssl,psl,rtmp,schannel,ssh,ssl,sspi,tool,websockets,winidn,wolfssl,zstd]:x64-windows --overlay-ports=C:\Projects\LibQurl\overlays\openssl --x-install-root=C:\Projects\LibQurl\bin --recurse
-
-COPY C:\Projects\LibQurl\bin\x64-windows\tools\curl\*.dll bin\
-COPY C:\Projects\LibQurl\bin\x64-windows\tools\libmagic\share\misc\*.mgc bin\
-COPY C:\Projects\LibQurl\bin\x64-windows\tools\libmagic\bin\* bin\
+COPY C:\Projects\LibQurl\build\x64-windows\tools\curl\*.dll bin\
+COPY C:\Projects\LibQurl\build\x64-windows\tools\libmagic\share\misc\*.mgc bin\
+COPY C:\Projects\LibQurl\build\x64-windows\tools\libmagic\build\* bin\
